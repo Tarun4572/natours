cf61d4fa2e733fcf0ee4a653a334bab8db344eae
pottigari.tarun <pottigaritarun01@gmail.com>
2023-12-23 19:01:24 +0530

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const app = express();
+
+// 1) Middlewares
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This Route is not implemented',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This Route is not implemented',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This Route is not implemented',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This Route is not implemented',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This Route is not implemented',
+  });
+};
