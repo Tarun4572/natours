a1aad76c09e2dc2f530e8835982447e57e8c206a
pottigari.tarun <pottigaritarun01@gmail.com>
2024-01-24 21:55:04 +0530

    implemented Email handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -51,7 +51,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  createSendToken(newUser, 200, res);
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
+
+  createSendToken(newUser, 201, res);
   // const token = signToken(newUser._id);
 
   // res.status(200).json({
@@ -170,11 +173,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a patch Request with your new password and passwordConfirm to: ${resetURL}.\n If you din't forget your password, please ignore this!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'your Password Reset Token',
-      message
-    });
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'Success',
--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,66 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split('')[0];
+    this.url = url;
+    this.from = `Tarun Pottigari <${process.env.EMAIL_FROM}`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      // there is problem with SendGrid signup. but process is same, just have to create account and provide necessary
+      // credentials in createTransport method.
+      return 1;
     }
-    // Activate in gmail "less secure app" option
-  });
-  //2) Define Email options
-  const mailOptions = {
-    from: 'Tarun',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
-  //3) send the email with node mailer
-  await transporter.sendMail(mailOptions);
-};
 
-module.exports = sendEmail;
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html)
+    };
+
+    // 3) create a transport and send email
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
+};
